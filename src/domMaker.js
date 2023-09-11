fe70cf182810921bf0a0d8c6bb2b3d7c84d58fde
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-11 13:07:05 +0100

    Made wrapper dynamically & added logo element

--- src/domMaker.js
+++ src/domMaker.js
@@ -1,9 +1,13 @@
-export default (function() {
+//Import logos
+import pokpok from './food-drumstick.svg';
 
+export default (function() {    
     //Cache DOM
-    const wrapper = document.querySelector('#content');
+    const body = document.querySelector('body');
 
+    /*OUTTER ELEMENTS*/ 
     //Create elements
+    const wrapper = document.createElement('div');
     const sideBarEl = document.createElement('div');
     const mainEl = document.createElement('div');
 
@@ -11,16 +15,27 @@ export default (function() {
     const contentEl = document.createElement('div');
 
     //Give elements classes
+    wrapper.id = 'content';
     sideBarEl.classList.add('sidebar');
     mainEl.classList.add('main');
     bannerEl.classList.add('banner');
     contentEl.classList.add('content');
 
     //Append elements
+    body.appendChild(wrapper);
+
     wrapper.appendChild(sideBarEl);
     wrapper.appendChild(mainEl);
 
     mainEl.appendChild(bannerEl);
     mainEl.appendChild(contentEl);
-    
+
+    /*INNER ELEMENTS*/
+    //Create sidebar elements
+    const logo = document.createElement('img');
+
+    logo.src = pokpok;
+
+    sideBarEl.appendChild(logo);
+
 })();
