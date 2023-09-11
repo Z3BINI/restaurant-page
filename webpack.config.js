2acb1d71b9eb15dd703a60f6e9d5ddc6fbc82568
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-11 09:49:04 +0100

    Fixed typo

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),s
+    path: path.resolve(__dirname, 'dist'),
   },
 };
