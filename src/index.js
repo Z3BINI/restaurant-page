7cb5110db1eb845e7e69c0e981991b298f8efd94
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-12 10:12:02 +0100

    Clear HTML before load, added switching code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import structure from './domMaker.js';
 import home from './home-content.js';
+import about from './about-content.js';
+import menu from './menu-content.js';
 import './style.css';
 
 const listener = (function() {
@@ -17,7 +19,23 @@ const listener = (function() {
 
     //Change page function
     const changeContent = function(page) {
-        console.log(page);
+        //Clear current DOM content element
+        structure.contentEl.innerHTML = '';
+
+        switch(page) {
+            case 'HOME':
+                structure.contentEl.appendChild(home.contentWrapper);
+                structure.bannerEl.style.backgroundImage = `url('${home.mainBanner}')`;
+                break;
+            case 'MENU':
+                structure.contentEl.appendChild(menu.contentWrapper);
+                structure.bannerEl.style.backgroundImage = `url('${menu.mainBanner}')`;
+                break;
+            case 'ABOUT US':
+                structure.contentEl.appendChild(about.contentWrapper);
+                structure.bannerEl.style.backgroundImage = `url('${about.aboutBanner}')`;
+                break;
+        }
     }
 })();
 
